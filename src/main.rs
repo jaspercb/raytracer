c34025afaf6e969a3064867e684b026e2f3c2a1d
Jasper Chapman-Black <jasperchapmanblack@gmail.com>
2019-08-12 11:38:40 -0700

    Rays, simple camera, background

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
 mod ray;
 mod math;
 
-use crate::math::Rgb;
+use crate::math::{Vec3, Rgb};
 use crate::ray::Ray;
+
+fn color(ray: Ray) -> Rgb
+{
+    let unit_direction = ray.direction().normalized();
+    let t = 0.5 * (unit_direction.y + 1.0);
+    return (1.0-t) * Rgb {r: 1.0, g: 1.0, b: 1.0} + t * Rgb {r: 0.5, g: 0.7, b: 1.0};
+}
 
 fn main()
 {
     // let col : Ray = Ray::new(Vec3 {x: 1.0, y: 1.0, z: 1.0}, Vec3 {x: 0.0, y: 0.0, z: 0.0});
     let nx : u32 = 200;
     let ny : u32 = 100;
+
+    let lower_left_corner = Vec3 {x: -2.0, y: -1.0, z: -1.0};
+    let horizontal = Vec3 {x: 4.0, y: 0.0, z: 0.0};
+    let vertical = Vec3 {x: 0.0, y: 2.0, z: 0.0};
+    let origin = Vec3 {x: 0.0, y: 0.0, z: 0.0};
     println!("P3\n{0} {1}\n255\n", nx, ny);
     for j in (0..ny).rev()
     {
         for i in 0..nx
         {
-            let col : Rgb = Rgb {r: (i as f64)/(nx as f64), g: (j as f64)/(ny as f64), b: 0.2};
+            let u = (i as f64) / (nx as f64);
+            let v = (j as f64) / (ny as f64);
+            let r: Ray = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical);
+            let col = color(r);
             let ir = (255.99 * col.r) as u32;
             let ig = (255.99 * col.g) as u32;
             let ib = (255.99 * col.b) as u32;
--- src/ray.rs
+++ src/ray.rs
@@ -13,4 +13,12 @@ impl Ray {
     pub fn at(&self, t: f64) -> Vec3 {
         return self.origin + t * self.direction;
     }
+
+    pub fn origin(&self) -> Vec3 {
+        self.origin
+    }
+
+    pub fn direction(&self) -> Vec3 {
+        self.direction
+    }
 }
